4817a21d05335e05b96f88e39522e32f4349287a
sush <sushmasriya19@gmail.com>
2024-08-12 15:12:37 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,28 @@
+//longest word in a string taking the string as an input
+
+
+//NOTES
+//IF STIRNG EMPTY OR ONLY SPACE RETURN FALSE
+//IF STRING REMOVE TRAILING AND FOLLOWING SPACE
+//IF STRING REMOVE LEADING AND TRAILING SPACE
+const longestWord=(str)=>{
+    str=str.trim()
+    
+    if(str.trim().length===0)
+    {
+        return false;
+    }
+    let words=str.split(" ")
+    words=words.sort((a,b)=>(a.length-b.length));
+    console.log(words)
+return words.at(-1)
+  
+  
+  
+
+
+}
+
+console.log(
+    longestWord("Hello world , this is a test string"),
+)
